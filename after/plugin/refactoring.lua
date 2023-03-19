0de27a95e863d0e6e7c669fe6ddc1e5990591668
Philip Kimathi <kimathi.phil@gmail.com>
2023-03-19 22:34:16 +0300

    feat!: Change plugin manager to lazy.nvim

--- after/plugin/refactoring.lua
+++
@@ -1,8 +0,0 @@
-require('refactoring').setup({})
-
--- Remaps for the refactoring operations currently offered by the plugin
-vim.api.nvim_set_keymap("v", "<leader>re", [[ <Esc><Cmd>lua require('refactoring').refactor('Inline Variable')<CR>]], {noremap = true, silent = true, expr = false})
-
--- Inline variable can also pick up the identifier currently under the cursor without visual mode
-vim.api.nvim_set_keymap("n", "<leader>re", [[ <Cmd>lua require('refactoring').refactor('Inline Variable')<CR>]], {noremap = true, silent = true, expr = false})
-
